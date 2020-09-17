616262c47577f855cb882cb8f044dcb7aeb8545b
Jack Perala <peralaj94@gmail.com>
2020-09-17 11:04:21 -0500

    Write the logic to place all consenants
    Wrote the code for disemvowel.c to return the array containing
    all consenants after removing the vowels.

--- disemvowel/disemvowel.c
+++ disemvowel/disemvowel.c
@@ -1,5 +1,5 @@
 #include <stdlib.h>
-
+#include <string.h>
 #include "disemvowel.h"
 
 char *disemvowel(char *str) {
@@ -7,6 +7,8 @@ char *disemvowel(char *str) {
   // Declaring variable for the string length.
   // This length will also be used to allocate space in the array.
   int len, i;
+  // This new length will allocate space in the array for the altered word.
+  int newLen,k;
   // Used to hold the original string to parse through looking for vowels.
   char *strHolder;
   // Where the altered string will be placed to return at the end.
@@ -14,6 +16,9 @@ char *disemvowel(char *str) {
 
   // Current length of the str input.
   len = strlen(str);
+  // Setting the string keeping track of the original string
+  // Without vowels to the length of the original string.
+  newLen = len;
 
   // Array to hold the original str input for parsing.
   strHolder = (char*) calloc(len + 1, sizeof(char));
@@ -22,7 +27,7 @@ char *disemvowel(char *str) {
   // This will then de-increment the len variable so we can allocate an
   // Appropriate amount of space in the array for the new string.
   // Incrementing the i variable using prefix might cause issues, use postfix?
-  for (i = 0; i < len; ++i) {
+  for (i = 0; i < len; i++) {
 
 	// Disgusting if statement to check for uppercase or lowercase
 	// vowels and index i.
@@ -39,21 +44,46 @@ char *disemvowel(char *str) {
 	     | strHolder[i] == 'O' 
 	     | strHolder[i] == 'U'
 				  ){
-	--len;
+	newLen--;
 	}
   }
   
   // Freeing the array used to hold the initial str input.
   // Actually probably need this later but I can come back to it.
-  free(strHolder);
 
   // Setting up the result array with an amount of space appropriate for after
   // the original str input has been disemvoweled.
-  result = (char*) calloc(len + 1, sizeof(char));
+  result = (char*) calloc(newLen + 1, sizeof(char));
 
-  for(i = 0; i < len; ++i) {
-  	
+  // Variable to keep track of the indice of the result array.
+  int ind, j;
+  ind = 0;  
+
+  for(i = 0; i < len; i++) {
+  	if (strHolder[i] != 'a' 
+             | strHolder[i] != 'e' 
+	     | strHolder[i] != 'i'  
+	     | strHolder[i] != 'o' 
+	     | strHolder[i] != 'u' 
+	     | strHolder[i] != 'A' 
+	     | strHolder[i] != 'E' 
+	     | strHolder[i] != 'I' 
+	     | strHolder[i] != 'O' 
+	     | strHolder[i] != 'U'
+				  ){
+	result[ind] = strHolder[i];
+	ind++;
   }
+}
+
+  result[len] = '\0';
 
   return result;
 }
+
+
+
+
+
+
+
